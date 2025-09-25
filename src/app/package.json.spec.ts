98e1be72fd5f8bbef1f90cb62246f22602d0eee3
dustin.turner444 <dustin.turner444@gmail.com>
2025-09-25 03:23:09 +0000

    Changes before Firebase Studio auto-run

--- src/app/package.json.spec.ts
+++ src/app/package.json.spec.ts
@@ -46,3 +46,104 @@ describe('package.json', () => {
     expect(packageJson.devDependencies['typescript']).toBeDefined();
   });
 });
+describe('tsconfig.json', function (): void {
+  it('should have compileOnSave set to false', () => {
+    expect(tsconfig.compileOnSave).toBe(false);
+  });
+
+  describe('compilerOptions', () => {
+    it('should have the correct baseUrl', () => {
+      expect(tsconfig.compilerOptions.baseUrl).toBe('./');
+    });
+
+    it('should have the correct outDir', () => {
+      expect(tsconfig.compilerOptions.outDir).toBe('./dist/out-tsc');
+    });
+
+    it('should have forceConsistentCasingInFileNames set to true', () => {
+      expect(tsconfig.compilerOptions.forceConsistentCasingInFileNames).toBe(true);
+    });
+
+    it('should have strict set to true', () => {
+      expect(tsconfig.compilerOptions.strict).toBe(true);
+    });
+
+    it('should have noImplicitOverride set to true', () => {
+      expect(tsconfig.compilerOptions.noImplicitOverride).toBe(true);
+    });
+
+    it('should have noPropertyAccessFromIndexSignature set to true', () => {
+      expect(tsconfig.compilerOptions.noPropertyAccessFromIndexSignature).toBe(true);
+    });
+
+    it('should have noImplicitReturns set to true', () => {
+      expect(tsconfig.compilerOptions.noImplicitReturns).toBe(true);
+    });
+
+    it('should have noFallthroughCasesInSwitch set to true', () => {
+      expect(tsconfig.compilerOptions.noFallthroughCasesInSwitch).toBe(true);
+    });
+
+    it('should have sourceMap set to true', () => {
+      expect(tsconfig.compilerOptions.sourceMap).toBe(true);
+    });
+
+    it('should have declaration set to false', () => {
+      expect(tsconfig.compilerOptions.declaration).toBe(false);
+    });
+
+    it('should have downlevelIteration set to true', () => {
+      expect(tsconfig.compilerOptions.downlevelIteration).toBe(true);
+    });
+
+    it('should have experimentalDecorators set to true', () => {
+      expect(tsconfig.compilerOptions.experimentalDecorators).toBe(true);
+    });
+
+    it('should have moduleResolution set to "node"', () => {
+      expect(tsconfig.compilerOptions.moduleResolution).toBe('node');
+    });
+
+    it('should have importHelpers set to true', () => {
+      expect(tsconfig.compilerOptions.importHelpers).toBe(true);
+    });
+
+    it('should have target set to "ES2022"', () => {
+      expect(tsconfig.compilerOptions.target).toBe('ES2022');
+    });
+
+    it('should have module set to "ES2022"', () => {
+      expect(tsconfig.compilerOptions.module).toBe('ES2022');
+    });
+
+    it('should have useDefineForClassFields set to false', () => {
+      expect(tsconfig.compilerOptions.useDefineForClassFields).toBe(false);
+    });
+
+    it('should have the correct lib', () => {
+      expect(tsconfig.compilerOptions.lib).toEqual(['ES2022', 'dom']);
+    });
+  });
+
+  describe('angularCompilerOptions', () => {
+    it('should have enableI18nLegacyMessageIdFormat set to false', () => {
+      expect(tsconfig.angularCompilerOptions.enableI18nLegacyMessageIdFormat).toBe(false);
+    });
+
+    it('should have strictInjectionParameters set to true', () => {
+      expect(tsconfig.angularCompilerOptions.strictInjectionParameters).toBe(true);
+    });
+
+    it('should have strictInputAccessModifiers set to true', () => {
+      expect(tsconfig.angularCompilerOptions.strictInputAccessModifiers).toBe(true);
+    });
+
+    it('should have strictTemplates set to true', () => {
+      expect(tsconfig.angularCompilerOptions.strictTemplates).toBe(true);
+    });
+
+    it('should have the correct _enabledBlockTypes', () => {
+      expect(tsconfig.angularCompilerOptions._enabledBlockTypes).toEqual(['if', 'for']);
+    });
+  });
+});
